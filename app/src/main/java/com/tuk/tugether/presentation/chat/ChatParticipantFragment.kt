942d987e18ce2a882faf8ba457d2bd0078e7b32d
Eouls <163132896+Eouls@users.noreply.github.com>
2025-07-15 01:36:45 +0900

    [UI/#8] 채팅 UI 구현

---
+++ app/src/main/java/com/tuk/tugether/presentation/chat/ChatParticipantFragment.kt
@@ -0,0 +1,16 @@
+package com.tuk.tugether.presentation.chat
+
+import com.tuk.tugether.R
+import com.tuk.tugether.databinding.FragmentChatParticipantBinding
+import com.tuk.tugether.presentation.base.BaseFragment
+
+class ChatParticipantFragment: BaseFragment<FragmentChatParticipantBinding>(R.layout.fragment_chat_participant) {
+
+    override fun initObserver() {
+
+    }
+
+    override fun initView() {
+
+    }
+}
---
+++ app/src/main/java/com/tuk/tugether/presentation/chat/ChatRoomActivity.kt
@@ -0,0 +1,16 @@
+package com.tuk.tugether.presentation.chat
+
+import com.tuk.tugether.R
+import com.tuk.tugether.databinding.ActivityChatRoomBinding
+import com.tuk.tugether.presentation.base.BaseActivity
+
+class ChatRoomActivity: BaseActivity<ActivityChatRoomBinding>(R.layout.activity_chat_room) {
+
+    override fun initObserver() {
+
+    }
+
+    override fun initView() {
+
+    }
+}
