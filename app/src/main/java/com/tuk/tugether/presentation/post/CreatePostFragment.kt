5791bcea74908badae5e8dc2fbfc0e2aacd8044c
ekgml001 <163154446+ekgml001@users.noreply.github.com>
2025-07-15 14:30:46 +0900

    [UI/#13] createPost xml 작성

---
+++ app/src/main/java/com/tuk/tugether/presentation/post/CreatePostFragment.kt
@@ -0,0 +1,18 @@
+package com.tuk.tugether.presentation.post
+
+import com.tuk.tugether.R
+import com.tuk.tugether.databinding.FragmentCreatePostBinding
+import com.tuk.tugether.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CreatePostFragment: BaseFragment<FragmentCreatePostBinding>(R.layout.fragment_create_post) {
+
+    override fun initView() {
+
+    }
+
+    override fun initObserver() {
+
+    }
+}
