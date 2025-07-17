459599868bb05a22dbb73550b7008f7906949f48
ekgml001 <163154446+ekgml001@users.noreply.github.com>
2025-07-17 22:35:04 +0900

    [FIX/#25] merge 오류 해결

--- app/src/main/java/com/tuk/tugether/di/RepositoryModule.kt
+++ app/src/main/java/com/tuk/tugether/di/RepositoryModule.kt
@@ -3,13 +3,10 @@ package com.tuk.tugether.di
 import android.app.Application
 import android.content.Context
 import com.tuk.tugether.data.repositoryImpl.NotificationRepositoryImpl
-import com.tuk.tugether.data.repositoryImpl.TestRepositoryImpl
 import com.tuk.tugether.data.repositoryImpl.PostRepositoryImpl
+import com.tuk.tugether.data.repositoryImpl.TestRepositoryImpl
 import com.tuk.tugether.data.service.TestService
 import com.tuk.tugether.domain.repository.NotificationRepository
-import com.tuk.tugether.data.repositoryImpl.TestRepositoryImpl
-import com.tuk.tugether.data.repositoryImpl.post.PostRepositoryImpl
-import com.tuk.tugether.data.service.TestService
 import com.tuk.tugether.domain.repository.PostRepository
 import com.tuk.tugether.domain.repository.TestRepository
 import dagger.Module
